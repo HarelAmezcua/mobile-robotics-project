28176df6dae1cbbf582c056e68b6a9ade8da0ab2
Harel Hernandez <harelamezcua@gmail.com>
2024-11-16 18:55:57 -0600

    Changes made

--- test_odometry/initialize_odometry_test_parameters.py
+++ test_odometry/initialize_odometry_test_parameters.py
@@ -5,7 +5,7 @@ def initialize_parameters():
     L = 0.25
     l = 0.20
     dt = 0.01
-    Tf = 100
+    Tf = 20
     N = int(Tf / dt)
     gain_matrix = np.diag([10,10, 10])  # [k_x, k_y, k_theta]
     return L, l, dt, Tf, N, gain_matrix
@@ -15,19 +15,19 @@ def initialize_parameters():
 import numpy as np
 
 def initialize_states(N):
-    q = np.array([0, 0, 0])
-    qp = np.zeros(3)
-    q_plot = np.zeros((3, N))
-    qp_plot = np.zeros((3, N))
-    control_plot = np.zeros((3, N))
-    wheel_speed_plot = np.zeros((4, N))
-    q_desired_plot = np.zeros((3, N))
-    qp_desired_plot = np.zeros((3, N))
+    visual_pose = np.array([0, 0, 0])
+    odometry_pose = np.zeros((3,1))
+
+    visual_pose_plot = np.zeros((3, N))
+    odometry_pose_plot = np.zeros((3,N))
+
     t_plot = np.zeros(N)
     dt_plot = np.zeros(N)
-    q_plot[:, 0] = q
-    qp_plot[:, 0] = qp
-    return q, qp, q_plot, qp_plot, control_plot, wheel_speed_plot, q_desired_plot, qp_desired_plot, t_plot, dt_plot
+
+    visual_pose_plot[:, 0] = visual_pose
+    odometry_pose_plot[:, 0] = odometry_pose
+
+    return visual_pose, odometry_pose,visual_pose_plot,odometry_pose_plot, t_plot, dt_plot
 
 
 # plot_results.py
@@ -35,7 +35,7 @@ import matplotlib.pyplot as plt
 from robot_functions import plotting_xy, plot_wheel_speeds, plot_control_action, plot_desired_vs_actual
 
 def plot_results(q_plot, q_desired_plot, t_plot, control_plot, wheel_speed_plot, dt_plot):
-    plotting_xy(q_plot, q_desired_plot)
+    plotting_xy(q_plot)
     plot_desired_vs_actual(t_plot, q_plot, q_desired_plot)
     plot_control_action(t_plot, control_plot)
     plot_wheel_speeds(t_plot, wheel_speed_plot)
